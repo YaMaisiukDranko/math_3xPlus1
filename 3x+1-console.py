1a0b02329319e66f03fd0729b673214825380a3f
Yaroslav <yarikmeisan@yandex.com>
2022-12-01 21:59:40 +0300

    Console version work

--- 3x+1-console.py
+++ 3x+1-console.py
@@ -1,11 +1,19 @@
-initialNum = int(input("Initial Number:  "))
-num = initialNum
-while num != 1:
-    if num % 2 == 0:
-        num /= 2
-        print(int(num))
-    else:
-        num = num * 3 + 1
-        print(int(num))
+num = 7
 
-print("Next number")
+def main():
+    global num
+    while num != 1:  # Use while num != 1
+        # Rules:
+        if num % 2 == 0:  # Check parity
+            num = num / 2
+            print(int(num))
+
+        elif num % 2 != 0 :
+            num = num * 3 + 1
+            print(int(num))
+
+main()
+
+
+
+
